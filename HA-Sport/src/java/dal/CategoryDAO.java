d0086abe0f96b929afb47e91535376a9135f5133
Dominic1802 <115022066+Dominic1802@users.noreply.github.com>
2023-04-23 22:35:13 +0700

    shop,search

--- HA-Sport/src/java/dal/CategoryDAO.java
+++ HA-Sport/src/java/dal/CategoryDAO.java
@@ -3,12 +3,30 @@ package dal;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import model.Category;
 
 public class CategoryDAO extends DBContext {
 
     PreparedStatement ps;
     ResultSet rs;
+
+    public List<Category> getAllCategory() {
+        List<Category> list = new ArrayList<>();
+
+        try {
+            String sql = "select * from Categories";
+            ps = connection.prepareStatement(sql);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                Category category = new Category(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4));
+                list.add(category);
+            }
+        } catch (SQLException e) {
+        }
+        return list;
+    }
 
     public Category getCategoryById(int id) {
         String sql = "select * from categories where catId = " + id;
--- HA-Sport/src/java/dal/ProductDAO.java
+++ HA-Sport/src/java/dal/ProductDAO.java
@@ -15,10 +15,11 @@ import model.Product;
  *
  * @author 84868
  */
-public class ProductDAO extends DBContext{
+public class ProductDAO extends DBContext {
+
     PreparedStatement ps;
     ResultSet rs;
-    
+
     public Product getProductById(int id) {
         String sql = "select * from products where proId=" + id;
         try {
@@ -41,7 +42,7 @@ public class ProductDAO extends DBContext{
         }
         return null;
     }
-    
+
     public List<Product> getAllProduct() {
         List<Product> list = new ArrayList<>();
         String sql = "select * from Products";
@@ -67,4 +68,97 @@ public class ProductDAO extends DBContext{
         }
         return list;
     }
+
+    public List<Product> getAllProductByConstrain(int index, int order_by, int CategoryID, long begin, long end, String name) {
+        List<Product> list = new ArrayList<>();
+        String sql = "SELECT * FROM Products WHERE 1=1";
+        sql = addCategoryID(sql, CategoryID);  //WHERE CategoryID=
+        sql = addUnitPrice(sql, begin, end);    //WHERE UnitsPrice BETWEEN
+        sql = addSearchByName(sql, name);
+        StringBuilder sb = new StringBuilder(sql);
+        switch (order_by) {
+            case 1: {
+                sb.append(" ORDER BY proName ASC OFFSET ? ROWS FETCH NEXT 6 ROWS ONLY");
+                break;
+            }
+            case 2: {
+                sb.append(" ORDER BY proPrice ASC OFFSET ? ROWS FETCH NEXT 6 ROWS ONLY");
+                break;
+            }
+            case 3: {
+                sb.append(" ORDER BY proPrice DESC OFFSET ? ROWS FETCH NEXT 6 ROWS ONLY");
+                break;
+            }
+            default: {
+                sb.append(" ORDER BY proId DESC OFFSET ? ROWS FETCH NEXT 6 ROWS ONLY");
+                break;
+            }
+        }
+        try {
+
+            ps = connection.prepareStatement(sb.toString());
+            ps.setInt(1, index * 6);
+            System.out.println("SQL: " + sb.toString());
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                Product product = new Product(rs.getInt(1),
+                        rs.getString(2),
+                        rs.getDouble(3),
+                        rs.getString(4),
+                        rs.getInt(5),
+                        rs.getDate(6),
+                        rs.getString(8),
+                        (new CategoryDAO()).getCategoryById(rs.getInt(7)),
+                        rs.getInt(9), 0);
+                list.add(product);
+
+            }
+        } catch (Exception e) {
+        }
+        return list;
+    }
+ // Extension
+    private String addCategoryID(String sql, int CategoryID) {
+        StringBuilder sb = new StringBuilder(sql);
+        if (CategoryID > 0) {
+            sb.append(" AND catId=").append(CategoryID);
+        }
+        return sb.toString();
+    }
+
+    private String addUnitPrice(String sql, long begin, long end) {
+        StringBuilder sb = new StringBuilder(sql);
+        if (end > 0) {
+            sb.append(" AND proPrice BETWEEN ").append(begin).append(" AND ").append(end);
+        }
+        return sb.toString();
+    }
+
+    private String addSearchByName(String sql, String name) {
+        StringBuilder sb = new StringBuilder(sql);
+        if (!name.isEmpty()) {
+            sb.append(" AND proName like '%").append(name).append("%'");
+        }
+        return sb.toString();
+    }
+
+    public int getPageCount(int CategoryID, long begin, long end, String name) {
+        int st = 0;
+        String sql = "SELECT COUNT(*) FROM Products WHERE 1=1";
+        sql = addCategoryID(sql, CategoryID);
+        sql = addUnitPrice(sql, begin, end);
+        sql = addSearchByName(sql, name);
+        try {
+            ps = connection.prepareStatement(sql);
+            rs = ps.executeQuery();
+            if (rs.next()) {
+                st = (int) Math.ceil(rs.getInt(1) / 6.0);
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
+
+        return st;
+    }
+
 }
