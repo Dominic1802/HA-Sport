c7d11a1b54e9db5a3431c121af4a8e3d3defb166
Dominic1802 <115022066+Dominic1802@users.noreply.github.com>
2023-04-25 01:40:36 +0700

    payment

---
+++ HA-Sport/src/java/controller/PayServlet.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import dal.AccountDAO;
+import dal.OrderDAO;
+import dal.ProductDAO;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import model.Account;
+import model.Order;
+import model.Product;
+
+/**
+ *
+ * @author Admin
+ */
+@WebServlet(name = "PayServlet", urlPatterns = {"/pay"})
+public class PayServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        if (session.getAttribute("accountsession") == null) {
+            response.sendRedirect("login");
+            return;
+        } else {
+            response.sendRedirect("Information.jsp");
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html; charset=UTF-8");
+        HttpSession session = request.getSession();
+        if (session.getAttribute("accountsession") == null) {
+            response.sendRedirect("login");
+            return;
+        }
+        Account account = (Account) session.getAttribute("accountsession");
+        OrderDAO orderDAO = new OrderDAO();
+        Order order = orderDAO.getCurrentCart(account.getAccountId());
+        String fullName = request.getParameter("fullName");
+        String phone = request.getParameter("phone");
+        String email = request.getParameter("email");
+        String address = request.getParameter("address");
+        AccountDAO acc = new AccountDAO();
+        acc.setCustomer(account, email, fullName, phone, address);
+        ProductDAO productDAO = new ProductDAO();
+        for (Product product : order.getProIds()) {
+            productDAO.addSelled(product);
+        }
+        orderDAO.changeCartToOrder(order.getOrderId());
+        response.sendRedirect("home");
+    }
+}
